3fa4fb3e34d1e493122f5e9d763bbbc235d392f3
keeponZhang <462789909@qq.com>
2019-05-01 01:00:59 +0800

    no message

--- app/src/main/java/com/demo/lizejun/dagger2sample/chapter4/my/MyDataModule.java
+++ app/src/main/java/com/demo/lizejun/dagger2sample/chapter4/my/MyDataModule.java
@@ -10,7 +10,8 @@ public class MyDataModule {
     @Provides
     //module 的scope注解要与他的component的scope注解一致，所以如果其component是singleton,这里用PerScopeData会报错
 //    @PerScopeData
-    @PerScopeData
+//    @PerScopeData
+//    @Singleton
     public MyData provideMyData() {
         return new MyData("module");
     }
--- app/src/main/java/com/demo/lizejun/dagger2sample/chapter4/my/MyRepositoryComponent.java
+++ app/src/main/java/com/demo/lizejun/dagger2sample/chapter4/my/MyRepositoryComponent.java
@@ -5,7 +5,8 @@ import dagger.Subcomponent;
 
 //在组织关系中，如果父Component的@Scope不为@Singleton，那么子Component的@Scope可以为@Singleton,但是这里不行
 //@Singleton跟@Inject不能配合使用，不会实现单例，因为@Scope不能加载@Inject上面，如果通过module
-@PerScopeData
+//@PerScopeData
+//@Singleton
 @Subcomponent(modules = MyDataModule.class)
 public interface MyRepositoryComponent {
     public void inject(MyRepository myRepository);
